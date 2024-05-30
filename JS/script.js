956fac4573921632783b95418d196a59a3694438
CobaltWars <168757389+CobaltWars@users.noreply.github.com>
2024-05-30 19:49:33 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+// CHARGEMENT
+setTimeout(() => {
+	const preloader = document.querySelector(".preloader");
+	preloader.classList.add('fondu-out');
+setTimeout(() => {
+	preloader.style.display = "none";
+}, 500);
+}, 2070);
+
+// TRANSITION
+window.addEventListener("load", function() {
+	document.body.classList.add("active");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+var links = document.querySelectorAll("a");
+
+for (var i = 0; i < links.length; i++) {
+	var link = links[i];
+	var target = link.getAttribute("target");
+
+if (target !== "_blank") {
+	link.addEventListener("click", function(event) {
+		event.preventDefault();
+            var href = this.getAttribute("href");
+            document.body.classList.remove("active");
+            setTimeout(function() {
+            	window.location.href = href;
+            }, 500);
+        });
+}
+}
+});
