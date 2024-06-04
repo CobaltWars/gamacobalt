e81813ab1bb5b5efb164dc434744e2bde157ad1c
CobaltWars <168757389+CobaltWars@users.noreply.github.com>
2024-06-04 14:25:33 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,22 @@
-// CHARGEMENT
-setTimeout(() => {
-	const preloader = document.querySelector(".preloader");
-	preloader.classList.add('fondu-out');
-setTimeout(() => {
-	preloader.style.display = "none";
-}, 500);
-}, 2070);
-
-// TRANSITION
-window.addEventListener("load", function() {
-	document.body.classList.add("active");
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-var links = document.querySelectorAll("a");
-
-for (var i = 0; i < links.length; i++) {
-	var link = links[i];
-	var target = link.getAttribute("target");
-
-if (target !== "_blank") {
-	link.addEventListener("click", function(event) {
-		event.preventDefault();
-            var href = this.getAttribute("href");
-            document.body.classList.remove("active");
-            setTimeout(function() {
-            	window.location.href = href;
-            }, 500);
-        });
-}
-}
+$(document).ready(function () {
+    
+    AOS.init();
+    
+    $('#slide-toggle').click(function(){
+        $('.overlay').fadeToggle(50);
+        
+        $('#menu-slider').toggleClass('hide');
+        $('#main-warapper').toggleClass('left');
+        
+        $('body').toggleClass('no-scroll');
+    });  
+    
+    $('.overlay').click(function () {
+        $(this).fadeToggle(50);
+    
+        $('#menu-slider').toggleClass('hide');
+        $('#main-warapper').toggleClass('left');
+        
+        $('body').toggleClass('no-scroll');
+    });
 });
